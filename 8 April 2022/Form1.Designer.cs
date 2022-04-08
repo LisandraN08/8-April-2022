11e357c44de120fc7f9ed7d77e704b0082013472
LisandraN08 <lnicoline@student.ciputra.ac.id>
2022-04-08 11:07:14 +0700

    Add project files.

---
+++ 8 April 2022/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace _8_April_2022
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPlayer = new System.Windows.Forms.DataGridView();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.txtBoxQuery = new System.Windows.Forms.TextBox();
+            this.cBoxTeam = new System.Windows.Forms.ComboBox();
+            this.lblValue = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPlayer
+            // 
+            this.dgvPlayer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPlayer.Location = new System.Drawing.Point(13, 125);
+            this.dgvPlayer.Name = "dgvPlayer";
+            this.dgvPlayer.RowHeadersWidth = 51;
+            this.dgvPlayer.RowTemplate.Height = 24;
+            this.dgvPlayer.Size = new System.Drawing.Size(1474, 414);
+            this.dgvPlayer.TabIndex = 0;
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(657, 25);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(75, 30);
+            this.btnQuery.TabIndex = 1;
+            this.btnQuery.Text = "Query";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // txtBoxQuery
+            // 
+            this.txtBoxQuery.Location = new System.Drawing.Point(74, 29);
+            this.txtBoxQuery.Name = "txtBoxQuery";
+            this.txtBoxQuery.Size = new System.Drawing.Size(577, 22);
+            this.txtBoxQuery.TabIndex = 2;
+            // 
+            // cBoxTeam
+            // 
+            this.cBoxTeam.FormattingEnabled = true;
+            this.cBoxTeam.Location = new System.Drawing.Point(74, 82);
+            this.cBoxTeam.Name = "cBoxTeam";
+            this.cBoxTeam.Size = new System.Drawing.Size(121, 24);
+            this.cBoxTeam.TabIndex = 3;
+            this.cBoxTeam.SelectedIndexChanged += new System.EventHandler(this.cBoxTeam_SelectedIndexChanged);
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Location = new System.Drawing.Point(219, 85);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(0, 17);
+            this.lblValue.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1499, 551);
+            this.Controls.Add(this.lblValue);
+            this.Controls.Add(this.cBoxTeam);
+            this.Controls.Add(this.txtBoxQuery);
+            this.Controls.Add(this.btnQuery);
+            this.Controls.Add(this.dgvPlayer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPlayer;
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.TextBox txtBoxQuery;
+        private System.Windows.Forms.ComboBox cBoxTeam;
+        private System.Windows.Forms.Label lblValue;
+    }
+}
+
---
+++ 8 April 2022/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace _8_April_2022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public MySqlConnection sqlConnect = new MySqlConnection("server=localhost;uid=root;pwd=;database=premier_league");
+        public MySqlCommand sqlCommand;
+        public MySqlDataAdapter sqlAdapter;
+        public string sqlQuery;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dtTeam = new DataTable();
+            sqlQuery = "SELECT team_name as 'Nama Tim', team_id as 'ID_Team' FROM team";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dtTeam);
+            cBoxTeam.DataSource = dtTeam;
+            cBoxTeam.DisplayMember = "Nama Tim";
+            cBoxTeam.ValueMember = "ID_Team";
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            sqlConnect.Open();
+            sqlQuery = txtBoxQuery.Text;
+            DataTable dtPlayer = new DataTable();
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dtPlayer);
+            dgvPlayer.DataSource = dtPlayer;
+        }
+
+        private void cBoxTeam_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblValue.Text = cBoxTeam.SelectedValue.ToString();
+            DataTable dtPlayer = new DataTable();
+            sqlQuery = "SELECT * FROM player WHERE team_id = '" + cBoxTeam.SelectedValue.ToString() + "'";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dtPlayer);
+            dgvPlayer.DataSource = dtPlayer;
+        }
+    }
+}
